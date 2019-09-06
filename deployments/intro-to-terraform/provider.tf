7c0a43cc3330a0acd61c7ebe5d02caae88879efc
Matt Turner <matturner@gmail.com>
2019-09-06 12:20:34 +0100

    Version range for TF provider.

--- deployments/intro-to-terraform/provider.tf
+++ deployments/intro-to-terraform/provider.tf
@@ -1,16 +1,20 @@
 provider "google" {
   version = "~> 2"
+
   project = var.gcp_project_id
   region  = "europe-west1"
   zone    = "europe-west1-c"
 }
 
 provider "cloudflare" {
+  version = "~> 1"
+
   email = "matt.turner.wrk@gmail.com"
   token = var.cf_api_token
 }
 
 provider "aws" {
   version = "~> 2"
-  region  = "eu-west-1"
+
+  region = "eu-west-1"
 }
